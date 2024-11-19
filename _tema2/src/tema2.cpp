ede4c4bb3a8a0c34d5805f6f90d9ce240eaea5b4
Alex <alex_gabri02@yahoo.com>
2024-11-19 02:05:42 +0200

    commit 5

--- _tema2/src/tema2.cpp
+++ _tema2/src/tema2.cpp
@@ -13,6 +13,15 @@ public:
 	{
         cout << menu << " has " << nrAlergens<< " alergens." <<endl;
 	}
+
+        //calls default constructor
+	Breakfast() = default;
+	
+	//calls destructor
+	~Breakfast()
+	{
+	    cout<< menu << "has been destroyed" << endl;
+	}
 	
 	void get()
 	{
@@ -24,7 +33,16 @@ int main() {
         
         Breakfast EnglishBreakfast("Sausages, beans and eggs", 1);
 	EnglishBreakfast.get();
-        
 
+	Breakfast default_Breakfast;//see if default constructor works
+        
+        Breakfast AnotherBreakfast = EnglishBreakfast;//see if copy constructor works
+        AnotherBreakfast.get();
+	
+        
+        Breakfast YetAnotherBreakfast = move(EnglishBreakfast);//if move constructor works
+        YetAnotherBreakfast.get();
+	
+	
     return 0;
 }
